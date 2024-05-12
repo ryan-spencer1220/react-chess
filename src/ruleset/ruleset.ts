9b0516ef14c232c7405c1984e07b74a8d8cd1576
Your Name <youremail@example.com>
2024-05-11 17:03:08 -0700

    refactor pawn ruleset function

--- src/ruleset/ruleset.ts
+++ src/ruleset/ruleset.ts
@@ -6,7 +6,13 @@ import {
 
 export default class Ruleset {
   tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean {
-    return true;
+    const piece = boardState.find((p) => p.x === x && p.y === y);
+
+    if (piece) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isValidMove(
@@ -25,31 +31,22 @@ export default class Ruleset {
     console.log(`Team: ${team}`);
 
     if (type === PieceType.PAWN) {
-      if (team === TeamType.OUR) {
-        if (py === 1) {
-          if (px === x && (y - py === 1 || y - py === 2)) {
-            if (this.tileIsOccupied(x, y, boardState)) {
-            }
-            return true;
-          }
-        } else {
-          if (px === x && y - py === 1) {
-            return true;
-          }
+      const specialRow = team === TeamType.OUR ? 1 : 6;
+      const pawnDirection = team === TeamType.OUR ? 1 : -1;
+
+      if (px === x && py === specialRow && y - py === 2 * pawnDirection) {
+        if (
+          !this.tileIsOccupied(x, y, boardState) &&
+          !this.tileIsOccupied(x, y - pawnDirection, boardState)
+        ) {
+          return true;
         }
-      } else {
-        if (py === 6) {
-          if (px === x && (y - py === -1 || y - py === -2)) {
-            return true;
-          }
-        } else {
-          if (px === x && y - py === 1) {
-            return true;
-          }
+      } else if (px === x && y - py === pawnDirection) {
+        if (!this.tileIsOccupied(x, y, boardState)) {
+          return true;
         }
       }
     }
-
     return false;
   }
 }
